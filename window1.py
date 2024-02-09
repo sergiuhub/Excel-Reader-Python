097b3f54b963bdc4b25a36c50e3869dbb348a454
lojiktech <sergiu.secara3215@gmail.com>
2024-02-09 18:29:14 +0200

    Adding window1.py file --> will work more on this

---
+++ window1.py
@@ -0,0 +1,12 @@
+# First file fo the python file 
+
+import tkinter as tk    #importing the library for pop-up window
+
+window = tk.Tk()                             #initializez the creation of the GUI window
+window.configure(bg = "#EFAEA1")             #sets the color of the backround to a light brown
+window.geometry("1000x800")                  #sets the size of the GUI window to 1000x800
+window.title("Excel-Reader-Application")     #sets the title of the window to "Excel-Reader-Application"
+
+window.mainloop()
+
+
